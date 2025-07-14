c9e691e9df560bb9f794a26afd63d5e1593190f0
codewithfrancis <offor919@gmail.com>
2025-07-14 16:55:45 +0100

    feat: implement admin login with email and password validation

---
+++ server/services/auth/admin.service.js
@@ -0,0 +1,19 @@
+const prisma = require('../../config/db');
+const bcrypt = require("bcrypt")
+
+const loginAdmin = async (email, password) => {
+    const admin = await prisma.admin.findUnique({
+        where: {email}
+    })
+    if(!admin) {
+        throw new Error('Admin email not found')
+    }
+    const isMatch = await bcrypt.compare(password, admin.password)
+
+    if(!isMatch){
+        throw new Error('Password is incorrect');
+    }
+    return {id: admin.id, email: admin.email}
+}
+
+module.exports = loginAdmin;
