20edbfb6a8437b57f9825350385097c85c8762bf
codewithfrancis <offor919@gmail.com>
2025-07-14 17:40:04 +0100

    fix: cors issue

--- server/app.js
+++ server/app.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const authRoutes = require('./routes/auth.routes')
 const app = express();
 
 app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    credentials: true
+  }));
 
 //routes
 app.use("/api/auth", authRoutes )
