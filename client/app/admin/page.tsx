6d56b5544a7636680d7b5644d8f1aaa7ce73387a
codewithfrancis <offor919@gmail.com>
2025-07-14 18:09:44 +0100

    feat: add env file for api url config

--- client/app/admin/page.tsx
+++ client/app/admin/page.tsx
@@ -20,13 +20,14 @@ export default function LecturerLogin() {
     setIsLoading(true)
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/admin", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/admin`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ email, password }),
-      })
+      });
+      
 
       const data = await res.json()
 
